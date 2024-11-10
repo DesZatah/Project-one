f5db00030a732c4a2e27e7fa130b260f4285b8b4
DesZatah <desyisheeh@gmail.com>
2024-11-10 14:53:26 +0200

    Added README file for Net&DevTech Company website

---
+++ Home.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>desy</title>
+</head>
+<body>
+    <h1> my site 2</h1>
+</body>
+</html>
